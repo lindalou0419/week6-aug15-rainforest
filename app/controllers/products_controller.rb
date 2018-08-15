a04c3098f95bb7723a6c2b9ba768a6a9f2a867aa
yohanis-ke <yohanes.ke@gmail.com>
2018-08-15 12:20:16 -0400

    created aa new product form and controller part

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+
   end
 
   def create
+    @product = Product.new
+    @product.name = parms[:product][:name]
+    @product.description = parms[:product][:description]
+    @product.price_in_cents = parms[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   end
 
   def edit
