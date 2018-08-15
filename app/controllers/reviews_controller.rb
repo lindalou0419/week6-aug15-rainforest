0c6b5d05644aeefae9fc0988fbf5de0ccf4493fa
yohanis-ke <yohanes.ke@gmail.com>
2018-08-15 17:49:48 -0400

    Added New Review Form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,14 @@
 class ReviewsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
   end
 
   def edit
+    @review = Review.find(params[:id])
+    @product = Product.find(params[:product_id])
   end
 
   def update
