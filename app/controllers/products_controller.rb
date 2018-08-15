2cce4f444d8b0af41c7f1474d1f46f32032df8d6
ilyana19 <aries_2152@hotmail.com>
2018-08-15 15:38:09 -0400

    can now edit items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,9 +25,22 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      flash[:success] = "Product Updated!"
+      redirect_to product_url
+    else
+      flash[:error] = "Product Did Not Update!"
+      render :edit
+    end
   end
 
   def destroy
