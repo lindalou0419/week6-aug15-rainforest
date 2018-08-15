521c8b6925ba9e743299874ea72028ba5d4676ef
yohanis-ke <yohanes.ke@gmail.com>
2018-08-15 16:36:17 -0400

    Created review model and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :reviews
   validates :name, :description, :price_in_cents, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_141404) do
+ActiveRecord::Schema.define(version: 2018_08_15_203108) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price_in_cents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
