ab0b63c7ae728e7b08066f1b9c63b36ae2f60975
ilyana19 <aries_2152@hotmail.com>
2018-08-15 22:35:37 -0400

    added a link to edit review and pulls the correct data from db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,17 @@
 class ReviewsController < ApplicationController
   def create
-    @product = Product.find(params[:product_id])
+    # @product = Product.find(params[:product_id])
     @review = Review.new
     @review.comment = params[:review][:comment]
     @review.product_id = params[:product_id]
 
+    # redirects to the product page when it submitted
     if @review.save
       flash[:notice] = "New review added!"
-      redirect_to product_url(@product)
+      redirect_to "/products/#{params[:product_id]}"
     else
       flash[:notice] = "There was an error."
-      render 'products/show'
+      redirect_to "/products/#{params[:product_id]}"
     end
   end
 
