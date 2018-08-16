633b53d95a6e2da23c1bc4b21eaf576368b8bf0b
ilyana19 <aries_2152@hotmail.com>
2018-08-16 12:00:30 -0400

    fixed route resources and removed unecessary pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destory'
+  resource :sessions, only: %i(new create destroy)
   resource :users, only: %i(new create)
 
   resources :products do
     resources :reviews, except: [:index, :new, :show]
   end
-  
+
   root 'products#index'
 end
