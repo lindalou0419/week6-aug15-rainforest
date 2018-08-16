83c699c576754b7b8ba12997ef13a72a623de3fb
ilyana19 <aries_2152@hotmail.com>
2018-08-16 00:36:18 -0400

    fixed some paths and styled the forms etc

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   def create
-    # @product = Product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
     @review = Review.new
     @review.comment = params[:review][:comment]
     @review.product_id = params[:product_id]
@@ -8,10 +8,10 @@ class ReviewsController < ApplicationController
     # redirects to the product page when it submitted
     if @review.save
       flash[:notice] = "New review added!"
-      redirect_to "/products/#{params[:product_id]}"
+      redirect_to product_url(params[:product_id])
     else
       flash[:notice] = "There was an error."
-      redirect_to "/products/#{params[:product_id]}"
+      render 'products/show'
     end
   end
 
