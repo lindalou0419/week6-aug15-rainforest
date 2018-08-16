9464b396e81247324b0bebd8ca4db01eb53f1821
ilyana19 <aries_2152@hotmail.com>
2018-08-16 11:35:05 -0400

    generated resources/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :users, only: %i(new create)
+
   resources :products do
     resources :reviews, except: [:index, :new, :show]
   end
+  
   root 'products#index'
 end
