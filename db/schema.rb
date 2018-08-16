bb1cc559a962e1152335dfc1737ee54707ab21b6
ilyana19 <aries_2152@hotmail.com>
2018-08-16 11:21:20 -0400

    created user table and home page added a link to sign up and log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_203108) do
+ActiveRecord::Schema.define(version: 2018_08_16_145639) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2018_08_15_203108) do
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
     t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
